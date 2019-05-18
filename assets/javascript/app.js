b7a14edbdfaee65c3400400ae52eb4ea704ab81a
duneoz <nicholas.charles.meyers@gmail.com>
2019-05-18 07:31:44 -0700

    working on click events to prepend results of AJAX get method to the giphy API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,6 @@
 // Initial array of characters
 var characters = ["Bert", "Ernie", "Cookie Monster", "Big Bird"];
 
-// Generic function for capturing the character name from the data-attribute
-function alertMovieName() {
-
-  // YOUR CODE GOES HERE!!!
-
-}
-
 // Function for displaying character data
 function renderButtons() {
 
@@ -42,7 +35,7 @@ $("#add-character").on("click", function(event) {
 
   // check if character is truthy (has a value), push a button if so, else nothing
   if (character) {
-    // The movie from the textbox is then added to our array
+    // The character from the textbox is then added to our array
   characters.push(character);
   $("#character-input").val("");
   }
@@ -59,21 +52,44 @@ $("#add-character").on("click", function(event) {
 // We're adding the event listener to the document itself because it will
 // work for dynamically generated elements
 // $(".movies").on("click") will only add listeners to elements that are on the page at that time
-$(document).on("click", ".movie", alertMovieName);
+// $(document).on("click", ".character", alertMovieName);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
 
 
 //button click run AJAX get
-$("button").on("click", function() {
-    var person = $(this).attr("data-person");
+$(".character").on("click", function() {
+    console.log("on click event being heard");
+    var character = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      person + "&api_key=b6mnH3SZPhauWLEoC7W1irZoPRn1TsOl&limit=10";
+      character + "%Sesame%Street&api_key=b6mnH3SZPhauWLEoC7W1irZoPRn1TsOl&limit=10";
+    
+    console.log(queryURL);
 
     $.ajax({
       url: queryURL,
       method: "GET"
-    });
+    })
+
+    .then(function(response) {
+      var results = response.data;
+
+      for(var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var charImage = $("<img>");
+        charImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(charImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    })
 
 });
